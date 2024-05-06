7201384a120ce281cbf05993b83c24c64152d606
UntitledPenguin <qramisw@gmail.com>
2024-05-06 13:38:19 +0100

    Changes to be committed: 	modified:   data/siteMetadata.js

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,26 +1,19 @@
 /** @type {import("pliny/config").PlinyConfig } */
 const siteMetadata = {
-  title: 'Next.js Starter Blog',
-  author: 'Tails Azimuth',
-  headerTitle: 'TailwindBlog',
-  description: 'A blog created with Next.js and Tailwind.css',
+  title: 'UntitledPenguin Factory——某未知鹅工厂',
+  author: 'UntitledPenguin',
+  headerTitle: 'UntitledPenguin Factory',
+  description: 'Documentation of My Ideas, Ideating, Solutions and Overthinking.',
   language: 'en-us',
   theme: 'system', // system, dark or light
   siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
   siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
-  siteLogo: '/static/images/logo.png',
+  siteLogo: '/static/images/logoX.png',
   socialBanner: '/static/images/twitter-card.png',
-  mastodon: 'https://mastodon.social/@mastodonuser',
-  email: 'address@yoursite.com',
+  email: 'qramisw@gmail.com',
   github: 'https://github.com',
-  x: 'https://twitter.com/x',
   // twitter: 'https://twitter.com/Twitter',
-  facebook: 'https://facebook.com',
-  youtube: 'https://youtube.com',
-  linkedin: 'https://www.linkedin.com',
-  threads: 'https://www.threads.net',
-  instagram: 'https://www.instagram.com',
-  locale: 'en-US',
+  locale: 'en-UK',
   analytics: {
     // If you want to use an analytics provider you have to add it to the
     // content security policy in the `next.config.js` file.
