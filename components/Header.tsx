15828eb45754e5330b7794659259817b8e2db1f5
UntitledPenguin <qramisw@gmail.com>
2024-05-06 14:01:09 +0100

    Changes to be committed: 	modified:   components/Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -8,20 +8,23 @@ import SearchButton from './SearchButton'
 
 const Header = () => {
   return (
+    
     <header className="flex items-center justify-between py-10">
       <div>
-        <Link href="/" aria-label={siteMetadata.headerTitle}>
-          <div className="flex items-center justify-between">
-            <div className="mr-3">
-              <Logo />
-            </div>
-            {typeof siteMetadata.headerTitle === 'string' ? (
+      <Link href="/" aria-label={siteMetadata.headerTitle}>
+      <div className="flex items-center justify-between">
+        {siteMetadata.siteLogo && (
+          <div className="mr-3">
+            <img src={siteMetadata.siteLogo} alt="Logo"  style={{ width: '20%', height: 'auto' }} />
+              {typeof siteMetadata.headerTitle === 'string' ? (
               <div className="hidden h-6 text-2xl font-semibold sm:block">
                 {siteMetadata.headerTitle}
               </div>
             ) : (
               siteMetadata.headerTitle
             )}
+            </div>
+        )}
           </div>
         </Link>
       </div>
