3fcc978b8b1f11989a49e95247c503484db6849e
UntitledPenguin <qramisw@gmail.com>
2024-05-26 21:37:22 +0100

    modified:   data/blog/W002-GraphSearchAlgorithms.mdx 	modified:   data/blog/W003-Dijkstra.mdx 	modified:   data/blog/W401-DataStructureSummary.mdx 	modified:   data/projectsData.ts 	new file:   public/static/images/Sudoku.jpg 	new file:   public/static/images/Workflow.jpg

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -7,13 +7,13 @@ interface Project {
 
 const projectsData: Project[] = [
   {
-    title: 'Python小项目|A Sudoku Solver',
+    title: 'Python小项目| A Sudoku Solver',
     description: `A high-performance Sudoku solver utilizing Depth-First Search (DFS) and hash tables.Running time listed among top 1% on Leetcode. Click to take a look`,
     imgSrc: '/static/images/Sudoku.jpg',
     href: '/blog/W302-Sudoku',
   },
   {
-    title: 'A Computational Workflow for urban planning project data management',
+    title: 'GH工作流 | A Computational Workflow for urban planning project data management',
     description: `See the exploration of using Rhino and Grasshopper in my work as a masterplanner.`,
     imgSrc: '/static/images/workflow.jpg',
     href: 'https://www.linkedin.com/posts/jiaqi-wang-ramis_grasshopper-rhino-urbanplanningworkflow-activity-7199515469085638656-3a_Y?utm_source=share&utm_medium=member_desktop',
