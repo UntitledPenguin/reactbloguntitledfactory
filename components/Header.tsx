2aed8a037a812e6d24bb51dbb5666ef3962d95f5
UntitledPenguin <qramisw@gmail.com>
2024-10-05 21:21:04 +0100

    modified:   components/Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -37,7 +37,7 @@ const Header = () => {
               {link.title}
             </Link>
           ))}
-              <div className="flex items-center space-x-4 leading-5 sm:space-x-6">
+          
         {/* External Link to Aqua Playground */}
         <Link
           href="https://aquaplayground.vercel.app/"
